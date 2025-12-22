8b352296667e483670b08313ed2e6eeeeb0386b4
HarshvardhanPatil4700 <harshpatil4700@gmail.com>
2025-12-22 13:01:38 +0530

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,15 @@
+// GLOBALS  - NO WINDOW !!!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
+console.log("GLOBAL VARIABLES");
+console.log(__dirname);
+console.log(__filename);
+
+setInterval(() => {
+  console.log("hello world");
+}, 2000); // it runs the code block present inside it after every 2000 ms
